d2dd83300503ac6412d8afe5cdbf3edefa3fb3a1
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-09-18 21:37:01 +0530

    Leetcode grind(revision) - 18/09/23

---
+++ MaxAreaIsland.py
@@ -0,0 +1,46 @@
+'''
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+'''
+
+
+import collections
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        visited = set()
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
+            visited.add((r, c))
+            count = 1
+
+            while q:
+                row, col = q.popleft()
+
+                for dr, dc in directions:
+                    i, j = row + dr, col + dc
+                    if (i in range(rows) and
+                            j in range(cols) and
+                            grid[i][j] == 1 and
+                            (i, j) not in visited
+                            ):
+                        q.append((i, j))
+                        visited.add((i, j))
+                        count += 1
+
+            return count
+
+        ans = 0
+        for i in range(rows):
+            for j in range(cols):
+                if ((i, j) not in visited and grid[i][j] == 1):
+                    ans = max(ans, bfs(i, j))
+        return ans
---
+++ NumberOfIslands.py
@@ -0,0 +1,49 @@
+'''
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+'''
+import collections
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # In this solution I will apply a BFS algorithm
+
+        directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+        visited = set()
+        q = collections.deque()
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(i, j):
+            q.append([i, j])
+            count = 0
+
+            while q:
+                row, col = q.popleft()
+
+                if (row, col) in visited:
+                    continue
+
+                visited.add((row, col))
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if ((r, c) not in visited and
+                            r in range(0, rows) and
+                            c in range(0, cols) and
+                            grid[r][c] != '0'
+                            ):
+                        q.append([r, c])
+
+            # all 1's are over here, count of land increases by one
+            return 1
+
+        ans = 0
+        for i in range(rows):
+            for j in range(cols):
+                if ((i, j) not in visited and
+                        grid[i][j] != '0'
+                        ):
+                    ans += bfs(i, j)
+
+        return ans
