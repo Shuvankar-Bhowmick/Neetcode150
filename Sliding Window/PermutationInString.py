fc8050761e291018a093d897650a947e1319f316
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-12-04 21:34:48 +0530

    Added code for a sliding window problem

--- Sliding Window/PermutationInString.py
+++ Sliding Window/PermutationInString.py
@@ -16,37 +16,45 @@ from collections import defaultdict
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        set1, set2 = defaultdict(int), defaultdict(int)
-        length = len(set1)
+        if len(s2) < len(s1): return False
+
+        s1Count, s2Count = defaultdict(int), defaultdict(int)
         matches = 0
+        # Iterate through the first string
+        for i in range(len(s1)):
+            s1Count[s1[i]] += 1
+            s2Count[s2[i]] += 1
+
+        length = len(s1Count)
         
-        # Fill set1
-        for ch in s1:
-            set1[ch] += 1
-        # Fill set2
-        for ch in s2:
-            set2[ch] += 1
-            if set2[ch] == set1[ch]:
-                matches += 1        
-        
-        if matches == length:
-            return True
-        
+        for key in s1Count.keys():
+            if s1Count[key] == s2Count[key]:
+                matches += 1
+
         l = 0
-        for r in range(length, len(s2)):
+        for r in range(len(s1), len(s2)):
             if matches == length:
                 return True
             
-            if set2[s2[l]] == set1[s2[l]]:
-                matches -= 1
-            
-            set2[s2[l]] -= 1
-            set2[s2[r]] += 1
-            if set2[s2[r]] == set1[s2[r]]:
+            # check for the right pointer
+            s2Count[s2[r]] += 1
+            if s2Count[s2[r]] == s1Count[s2[r]]:
                 matches += 1
-                
-        return False        
-                
-            
-            
+            if (s2Count[s2[r]] == s1Count[s2[r]] + 1) and (s1Count[s2[r]] != 0):
+                matches -= 1
+
+            # check for the left pointer
+            s2Count[s2[l]] -= 1
+            if (s2Count[s2[l]] == s1Count[s2[l]]) and (s1Count[s2[l]] != 0):
+                matches += 1
+            if s2Count[s2[l]] == s1Count[s2[l]] - 1:
+                matches -= 1
+            l += 1
+
+        return True if matches == length else False
+
+solution = Solution()
+# print("Answer:", solution.checkInclusion("ab","eidboaoo"))         
+print("Answer:", solution.checkInclusion("ab",
+"eidboaoo"))         
                     
